9a4cdea703b6beb6d9591aaaf46568a1278b3fe6
Sergey Sammal <Sergey.Sammal@smartbear.com>
2015-12-04 11:52:33 +0300

    Performance improvements
    Scanning board >1000 times per second is possible
    
    Switch to use DIO2
    Arduino fast digitalWrite & digitalRead library
    http://www.codeproject.com/Articles/732646/Fast-digital-I-O-for-Arduino

--- chess_board/chess_board.ino
+++ chess_board/chess_board.ino
@@ -1,19 +1,21 @@
+#include <arduino2.h>
+
 // 75HC595 pins
-int dataOut_Pin = 5;   // green pin 14 on the 75HC595
-int latchOut_Pin = 6;  // green-white pin 12 on the 75HC595
-int clockOut_Pin = 7; // blue pin 11 on the 75HC595
+const int dataOut_Pin = 5;   // green pin 14 on the 75HC595
+const int latchOut_Pin = 6;  // green-white pin 12 on the 75HC595
+const int clockOut_Pin = 7; // blue pin 11 on the 75HC595
 
 // CD4014 pins
-int dataIn_Pin = 2;  // blue-white pin 3 (Q8) on the CD4014
-int clockIn_Pin = 3; // orange pin 10 (CLOCK) on the CD4014
-int latchIn_Pin = 4; // orange-white pin 9 (P/S Clock) on the CD4014
+const int dataIn_Pin = 2;  // blue-white pin 3 (Q8) on the CD4014
+const int clockIn_Pin = 3; // orange pin 10 (CLOCK) on the CD4014
+const int latchIn_Pin = 4; // orange-white pin 9 (P/S Clock) on the CD4014
 
-int button1_Pin = 12;
-int button2_Pin = 11;
+const int button1_Pin = 12; // button for white
+const int button2_Pin = 11; // button for black
 
-int boardChanged_event = 0xFF;
-int button1Pressed_event = 0xFE;
-int button2Pressed_event = 0xFD;
+const int boardChanged_event = 0xFF;
+const int button1Pressed_event = 0xFE;
+const int button2Pressed_event = 0xFD;
 
 boolean button1Pressed;
 boolean button2Pressed;
@@ -38,6 +40,8 @@ void setup(){
   for(int i = 0; i < 8; i++)
     boardData[i] = 0;
 
+  setNoLine();
+  
   // Serial
   Serial.begin(9600); 
 }
@@ -45,19 +49,74 @@ void setup(){
 void loop(){
 
   checkButtons();
-  
-  // Takes about 20 millis
   checkBoard();
   
-  
   delay(100);
+}
 
+void setNoLine() {
+  
+  digitalWrite(latchOut_Pin, LOW);
+  shiftOut(dataOut_Pin, clockOut_Pin, LSBFIRST, 0);
+  digitalWrite(latchOut_Pin, HIGH);
+}
+
+void setFirstLine(){
+
+  // Push 1
+  digitalWrite2(latchOut_Pin, LOW);
+  digitalWrite2(dataOut_Pin, HIGH);
+  digitalWrite2(clockOut_Pin, HIGH);
+  digitalWrite2(clockOut_Pin, LOW);
+  digitalWrite2(latchOut_Pin, HIGH);
+
+  // FOR switchLine
+  digitalWrite2(dataOut_Pin, LOW);
+}
+
+void switchLine() {
+
+  // dataOut_Pin should have LOW = 0
+  digitalWrite2(latchOut_Pin, LOW);
+  digitalWrite2(clockOut_Pin, HIGH);
+  digitalWrite2(clockOut_Pin, LOW);
+  digitalWrite2(latchOut_Pin, HIGH);
+}
+
+uint8_t shiftIn2(uint8_t dataPin, uint8_t clockPin) {
+  uint8_t value = 0;
+  uint8_t i;
+
+  for (i = 0; i < 8; ++i) {
+    digitalWrite2(clockPin, HIGH);
+    value |= digitalRead2(dataPin) << i;
+    digitalWrite2(clockPin, LOW);
+  }
+  return value;
+}
+
+byte scanLine2()
+{
+  //Pulse the latch pin:
+  //set it to 1 to collect parallel data
+  digitalWrite2(latchIn_Pin, HIGH);
+  // For cd4014
+  digitalWrite2(clockIn_Pin, HIGH);
+  //set it to 1 to collect parallel data, wait
+  delayMicroseconds(20);
+  //set it to 0 to transmit data serially  
+  digitalWrite2(latchIn_Pin, LOW);
+
+  //while the shift register is in serial mode
+  //collect each shift register into a byte
+  //the register attached to the chip comes in first 
+  return shiftIn2(dataIn_Pin, clockIn_Pin);
 }
 
 void setCurrentLine(int index){
 
   digitalWrite(latchOut_Pin, LOW);
-  shiftOut(dataOut_Pin, clockOut_Pin, LSBFIRST, 1 << index);
+  shiftOut(dataOut_Pin, clockOut_Pin, MSBFIRST, 1 << index);
   digitalWrite(latchOut_Pin, HIGH);
 }
 
@@ -81,7 +140,7 @@ byte scanLine()
 
 void checkButtons()
 {
-  if(digitalRead(button1_Pin) == LOW)
+  if(digitalRead2(button1_Pin) == LOW)
   {
     if(!button1Pressed)
       sendEvent(button1Pressed_event, "");
@@ -90,7 +149,7 @@ void checkButtons()
   else
     button1Pressed = false;
 
-  if(digitalRead(button2_Pin) == LOW)
+  if(digitalRead2(button2_Pin) == LOW)
   {
     if(!button2Pressed)
       sendEvent(button2Pressed_event, "");
@@ -100,15 +159,33 @@ void checkButtons()
     button2Pressed = false;
 }
 
+
+// --------------------------------------------
+// Check Board Details
+// --------------------------------------------
+
+// Use setCurrentLine & scanLine
+// Native arduino functions slow (3.4 uS for board scan)
+
+// Use setFirstLine & switchLine
+// Native and fast instead of shiftOut whole byte in setCurrentLine (1.3 uS for board scan)
+
+// Use setFirstLine & switchLine & scanLine2 & shiftIn2
+// Using DIO2 library which up to 5 times faster (event more performance is possible with using DigitalWrite2f)
+
+
 void checkBoard()
 {
   byte data[8];
+
+  setFirstLine();
   for(int i = 0; i < 8; i++)
   {
-    setCurrentLine(i);
-    data[i] = scanLine();
+    //setCurrentLine(i);
+    data[7-i] = scanLine2();
+    switchLine();
   }
-  
+
   boolean needSend = false;
   for(int i = 0; i < 8; i++)
     if (data[i] != boardData[i])
